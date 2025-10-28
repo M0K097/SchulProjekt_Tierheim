67c3943bfb688304572688bd732e3aadaa865530
Moritz Kolb <moritz.kolb@digitale-schule.saarland>
2025-10-28 09:08:16 +0100

    initializing base files

---
+++ Programmdateien/Tierhandlung_Projekt.1_Moritz_Kolb/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tierhandlung_Projekt._1_Moritz_Kolb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
