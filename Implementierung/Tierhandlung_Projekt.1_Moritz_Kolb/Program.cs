0ef9621bde8079a4b4d87eb0cefb05d04904ffc4
Moritz Kolb <moritz.kolb@digitale-schule.saarland>
2025-11-20 11:54:41 +0100

    addet tier hinzufügen functionality

--- Implementierung/Tierhandlung_Projekt.1_Moritz_Kolb/Program.cs
+++ Implementierung/Tierhandlung_Projekt.1_Moritz_Kolb/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Tierhandlung_Projekt._1_Moritz_Kolb
+﻿//Kurzbeschreibung: Eine Anwendung, mit der Mitarbeiter eines Tierheims die zu vermittelnden
+//Tiere mit Fotos und Beschreibungen präsentieren können.
+//Interessenten können die Tiere filtern und Adoptionsanfragen stellen.
+
+
+
+namespace Tierhandlung_Projekt._1_Moritz_Kolb
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Tierheim.tier_erstellen(); 
         }
     }
 }
--- Implementierung/Tierhandlung_Projekt.1_Moritz_Kolb/Tier.cs
+++ Implementierung/Tierhandlung_Projekt.1_Moritz_Kolb/Tier.cs
@@ -5,33 +5,38 @@ using System.Text;
 
 namespace Tierhandlung_Projekt._1_Moritz_Kolb
 {   
-    public enum tier_art
-    {
-        Hund,
-        Katze,
-        Hamster,
-        Ratte,
-        Maus,
-        Vogel,
-        Reptiel
-    }
-
     public static class Tierheim
     {
         static List<Tier> alle_tiere = new ();
-        static public void tier_hinzufügen(Tier tier) => alle_tiere.Add(tier);
-   
+        static private void tier_hinzufügen(Tier tier) => alle_tiere.Add(tier);
+        static public void tier_erstellen()
+        {
+            Console.WriteLine("Tierart:");
+            var art = Console.ReadLine();
+            Console.WriteLine("Tiername:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Geburtsdatum:");
+            var datum = Convert.ToDateTime(Console.ReadLine());
 
+            if (art != null && name != null)
+            {
+                tier_hinzufügen(new Tier(art, name, datum));
+            }
+            else
+            {
+                Console.WriteLine("Das Tier konnte nicht hinzugefügt werden");
+            }
+        }
     }
     public class Tier
     {
-        public tier_art tierart { get; set; }
+        public string tierart { get; set; }
         public string name { get; set; }
         public DateTime geburtsdatum { get; set; }
         public string beschreibung { get; set; }
         public bool reserviert { get; set; }
 
-        public Tier(tier_art art, string name_des_tieres, DateTime geburtsdatum)
+        public Tier(string art, string name_des_tieres, DateTime geburtsdatum)
         {
             tierart = art;
             name = name_des_tieres;
